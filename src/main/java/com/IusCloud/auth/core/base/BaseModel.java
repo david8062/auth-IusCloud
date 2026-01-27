74fffb68c0293e99a119a351730c782363a528cc
Jeffer <jeffersondavila1101@gmail.com>
2026-01-26 23:05:25 -0500

    Feature
    Config initial project

---
+++ src/main/java/com/IusCloud/auth/core/base/BaseModel.java
@@ -0,0 +1,41 @@
+package com.IusCloud.auth.core.base;
+
+import java.time.Instant;
+import java.util.UUID;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+public abstract class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    private Instant updatedAt;
+
+    private Instant deletedAt;
+
+    @Column(nullable = false)
+    private Boolean active = true;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = Instant.now();
+    }
+
+}
